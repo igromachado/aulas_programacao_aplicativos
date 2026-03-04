c6d2a9abb6d15eb186264163127faf5b61fef4ad
igromachado <igor.fam10@gmail.com>
2026-03-04 11:35:37 -0300

    aula7

--- 03. JavaScript/aula7/script.js
+++ 03. JavaScript/aula7/script.js
@@ -38,25 +38,59 @@ pessoas.push(pessoa3)
 pessoas.push(pessoa4)
 pessoas.push(pessoa5)
 
-const FilteredPeople = pessoas.filter((p) => p.age > 20)
-const FindIgo = pessoas.find((p) => p.name == 'igor')
+// const FilteredPeople = pessoas.filter((p) => p.age > 20)
+// const FindIgo = pessoas.find((p) => p.name == 'igor')
 
-console.log(FilteredPeople)
-console.log(FindIgo)
-console.log(pessoas.indexOf(FindIgo))
+// console.log(FilteredPeople)
+// console.log(FindIgo)
+// console.log(pessoas.indexOf(FindIgo))
 
-let container = document.getElementById('container')
+// let container = document.getElementById('container')
 
-function createCard(pessoa){
-    let div = document.createElement('div')
-    let span = document.createElement('span')
-    span.innerHTML = `${pessoa.name} ${pessoa.age}`
-    div.appendChild(span)
+// function createCard(pessoa){
+//     let div = document.createElement('div')
+//     let span = document.createElement('span')
+//     span.innerHTML = `${pessoa.name} ${pessoa.age}`
+//     div.appendChild(span)
 
-    return div
-}
+//     return div
+// }
 
-FilteredPeople.forEach(pessoa => {
-    const card = createCard(pessoa)
-    container.appendChild(card)
+// FilteredPeople.forEach(pessoa => {
+//     const card = createCard(pessoa)
+//     container.appendChild(card)
+// })
+
+// método filter -> filtrar de um vetor com base em uma condição
+// método find -> encontrar um valor com base em uma condição
+// método reduce -> reduz o vetor para um único valor
+
+const arr = [1,2,3,4,5]
+
+const somaArr = arr.reduce((acc, value) => {
+    return acc + value
+}, 0)
+
+console.log(somaArr)
+
+const avgAge = pessoas.reduce((acc, pessoa) => {
+    return acc + pessoa.age / pessoas.length
+}, 0)
+
+console.log(avgAge.toFixed(0))
+
+//map transforma um vetor de acordo com uma condição
+
+const arr2 = [1,2,3,4,5]
+
+const mappedArr = arr2.map((arr) => {
+    return arr * 2
 })
+
+console.log(mappedArr)
+
+const mappedPeople = pessoas.map((pessoa) => {
+    return {name: pessoa.name, lastname:  pessoa.lastname}
+})
+
+console.log(mappedPeople)
