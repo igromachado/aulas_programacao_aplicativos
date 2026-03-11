befae1b6f4b17acd76a3c2fc34f98e74f28d37fc
igromachado <igor.fam10@gmail.com>
2026-03-11 11:31:18 -0300

    aula10

---
+++ 03. JavaScript/aula10/script.js
@@ -0,0 +1,26 @@
+let data = []
+
+async function fetchData() { 
+    const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/distritos')
+    data = await response.json()
+}
+
+async function setData() {
+    await fetchData()
+
+    const element = document.getElementById('data')
+
+    const reduceData = data.reduce((acc,value) => {
+        if(value.municipio?.microrregiao?.mesorregiao?.UF?.sigla == 'PR'){
+            return acc + 1
+        }
+        else{
+            return acc
+        }
+    }, 0)
+
+    element.innerHTML = reduceData
+
+}
+
+setData()
---
+++ 03. JavaScript/aula10/script2.js
@@ -0,0 +1,27 @@
+let users = []
+let posts = []
+
+async function fetchData() { 
+  const userData = await fetch('https://jsonplaceholder.typicode.com/users')
+  const postsData = await fetch('https://jsonplaceholder.typicode.com/posts')
+  users = await userData.json()
+  posts = await postsData.json()
+}
+
+async function setData() {
+  await fetchData()
+  
+  users.forEach(user => {
+    console.log("User: " + user.username)
+
+    const usersPosts = posts.filter(post => post.userId === user.id)
+
+    usersPosts.forEach(post => {
+        console.log('Post: ' + post.title)
+    })
+
+    console.log('------------------')
+  })
+}
+
+setData()
