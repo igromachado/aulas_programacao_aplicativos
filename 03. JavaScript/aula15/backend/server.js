4be65acde4d9e46b8c998332d29fa7b949ef74d6
igromachado <igor.fam10@gmail.com>
2026-04-22 10:11:20 -0300

    aula15

--- 03. JavaScript/aula15/backend/server.js
+++ 03. JavaScript/aula15/backend/server.js
@@ -26,6 +26,61 @@ app.get('/', (req, res) => {
     return res.send('Servidor funcionando corretamente!')
 })
 
+app.get('/usuarios', async (req, res) => {
+    connection.query('SELECT * FROM usuarios', (err, results) =>{
+        if(err){
+            return
+        }
+        res.status(200).send(results)
+    })
+})
+
+app.get('/usuarios/:id', (req, res) => {
+    const {id} = req.params
+    connection.query('SELECT * FROM usuarios WHERE id = ?', 
+        [id], 
+        (err, results) =>{
+            if(err){
+                return
+            }
+            return res.status(200).send(results[0])
+        }
+    )
+})
+
+app.post('/registro', (req, res) => {
+    const {nome, email, senha} = req.body
+    connection.query('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)', 
+        [nome, email, senha])
+
+        return res.status(201).send({response: "Usuário cadastrado com sucesso!"})
+})
+
+app.delete('/deletar/:id', (req, res) => {
+    const {id} = req.params
+    try{
+        connection.query('DELETE FROM usuarios WHERE id = ?', [id])
+        return res.status(200).send({message: 'Usuário deletado com sucesso!'})
+    }
+    catch(e){
+        return res.status(500).send({error: e})
+    }
+})
+
+app.put('/atualizar/:id', (req,res) => {
+    const {id} = req.params
+    const {nome, email, senha} = req.body
+    try{
+        connection.query('UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?', 
+            [nome, email, senha, id]
+        )
+        return res.status(200).send({message: 'Usuário atualizado com sucesso!'})
+    }
+    catch{
+        return res.status(500).send({errpr: 'Ocorreu um erro ao atualizar'})
+    }
+})
+
 app.listen(port, () => {
     console.log('Servidor rodando em http://localhost:8080')
 })
---
+++ 03. JavaScript/aula15/sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE aula_add;
+USE aula_add;
+CREATE TABLE usuarios(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(60) NOT NULL,
+email VARCHAR(60) NOT NULL
+);
+
+select * from usuario
+
+
